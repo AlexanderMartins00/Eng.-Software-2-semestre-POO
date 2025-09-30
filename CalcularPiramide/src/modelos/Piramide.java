43cecb9389dff7b77a7ce35980631ad63e9ed2f0
AlexanderMartins00 <alexander062006@gmail.com>
2025-09-30 14:46:41 -0300

    Add files via upload

---
+++ CalcularPiramide/src/modelos/Piramide.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author alexander
+ */
+public class Piramide {
+    //Atributos
+    public float base = 0;
+    public float altura = 0;
+    
+    //Metodo
+    public float calcularVolume(){
+        return((float)(1.0/3.0) * base * altura);
+    }
+    
+}
+    
+
